e59cc4e59b7082954417bbe1b4a13cb29aaa1340
Nikola Jokic <jokicnikola07@gmail.com>
2024-01-30 18:33:48 +0100

    Init commit

---
+++ app.config.ts
@@ -0,0 +1,22 @@
+export default defineAppConfig({
+  docus: {
+    title: "BountyHub Docs",
+    description: "Documentation for bounty hub",
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: [],
+    },
+    main: {
+      padded: true,
+      fluid: true,
+    },
+    header: {
+      logo: false,
+      title: "BountyHub Docs",
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true,
+    },
+  },
+});
