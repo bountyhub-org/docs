2f92b04e2ad2549406a85a6763844f0c92fcb0f8
Nikola Jokic <jokicnikola07@gmail.com>
2024-05-23 15:42:15 +0200

    Add og tags to docs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,42 @@ export default defineNuxtConfig({
   extends: "@nuxt-themes/docus",
   devtools: { enabled: true },
   modules: [],
+  app: {
+    head: {
+      meta: [
+        {
+          property: "og:title",
+          content: "BountyHub Docs",
+        },
+        {
+          property: "og:site_name",
+          content: "BountyHub",
+        },
+        {
+          property: "og:description",
+          content: "BountyHub platform documentation",
+        },
+        {
+          property: "og:image",
+          content: "https://bountyhub.org/assets/logo_whitebg.png",
+        },
+        {
+          property: "og:image:width",
+          content: "500",
+        },
+        {
+          property: "og:image:height",
+          content: "500",
+        },
+        {
+          property: "og:url",
+          content: "https://docs.bountyhub.org",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+      ],
+    }
+  }
 });
